916de1b45bf3bb6756b8d051e90c3983ad054705
Jay <jay0@me.com>
2019-12-10 20:09:06 +0000

    Merge stack.c into project.c / implement stacks

--- project.c
+++ project.c
@@ -2,26 +2,46 @@
 #include <stdlib.h>
 #include <math.h>
 
-/* 
- * --------------------------
- * Prototype functions
- * --------------------------
+/*
+ * ----------------------------------------------
+ * Function/type prototypes
+ * ----------------------------------------------
  */
 
+// User interface
 int menu();
 double get_double_input(const char*);
 
-/* 
- * --------------------------
+// Implementation of stacks
+struct Stack;
+void PushStack(struct Stack*, char);
+char PopStack(struct Stack*);
+void DeleteStack(struct Stack*);
+
+/*
+ * ----------------------------------------------
+ * Struct definitions
+ * ----------------------------------------------
+ */
+
+struct Stack {
+    char *start; // Pointer to start of memory allocated for array of values
+    char *top; // Pointer to last defined value in array
+    int size; // Current size of array
+    int capacity; // Maximum size
+};
+
+/*
+ * ----------------------------------------------
  * Function definitions
- * --------------------------
+ * ----------------------------------------------
  */
 
 /*
- * --------------------------
  * Function: main()
- * --------------------------
+ *
  * Description: Main subroutine of the program. Executed on startup
+ * Parameters: none
  * Returns: Exit code, giving information about how the program performed (system dependant)
  */
 
@@ -67,11 +87,13 @@ int main() {
     }
 }
 
+// ------ User input functions ------
+
 /*
- * --------------------------
  * Function: menu()
- * --------------------------
+ *
  * Description: Displays a list of choices to the user
+ * Parameters: none
  * Returns: Integer representing choice selected. Guaranteed to be either 1 or 2
  */
 
@@ -98,13 +120,12 @@ int menu() {
 }
 
 /*
- * --------------------------
- * Function: get_double_input()
- * --------------------------
+ * Function: get_double_input(prompt)
+ * 
  * Description: Displays a prompt to the user (as passed to the function) and interprets input as a 
  *              double type - with some error checking
- * Parameters: char[] prompt - a character array (string) prompt which is given to printf() to be 
- *             shown to the user to inform their choice
+ * Parameters: prompt - a character array (string) prompt which is given to printf() 
+ *             to be shown to the user to inform their choice
  * Returns: double corresponding to interpretation (strtod). Will not return 0 unless the user 
  *          actually entered 0; invalid input is handled
  */
@@ -129,6 +150,76 @@ double get_double_input(const char* prompt) {
             return output;
         }
     }
+}
+
+// ------ Stack implementation functions ------
+
+/*
+ * Function: InitStack(capacity)
+ *
+ * Description: Factory method for creating and initializing stacks
+ * Parameters: capacity - the maximum length of the stack
+ * Returns: Empty stack with the capacity specified
+ */
+
+struct Stack InitStack(int capacity) {
+    struct Stack s;
+    s.start = malloc(capacity * sizeof(char)); // allocate a block of memory for the array
+    
+    if (s.start == NULL) { // malloc() returns null if it can't allocate, usually because of no RAM
+        printf("Unable to allocate memory for stack! Please check that you have enough RAM free.");
+        exit(EXIT_FAILURE); // Fatal error; program needs to quit
+    }
+
+    // Once malloc has succeeded, can now assign the other properties
+    s.capacity = capacity;
+    s.top = s.start - 1; // First value should be at s.start, and PushStack assigns to s.top + 1
+    s.size = 0;
+
+    return s;
+}
+
+/*
+ * Function: PushStack(stack, value)
+ *
+ * Description: Factory method for creating and initializing stacks
+ * Parameters: stack, the stack to push to
+ *             value, the value to push onto the stack
+ * Returns: none
+ */
+
+void PushStack(struct Stack *stack, char value) {
+    if (stack->size + 1 > stack->capacity) {
+        printf("Stack full! Value not pushed.");
+        return;
+    }
+
+    *(stack->top + 1) = value; // Write to the next address after the top of the stack
+    stack->top++; // Increment the pointers to the top and the size value accordingly
+    stack->size++;
+}
+
+/*
+ * Function: PopStack(stack)
+ *
+ * Description: Factory method for creating and initializing stacks
+ * Parameters: stack, the stack to pop from
+ * Returns: The value that was popped from the stack
+ */
+
+char PopStack(struct Stack *stack) {
+    if (stack->size == 0) {
+        printf("Stack is already empty! Value not popped.");
+    }
+    
+    char data;
+    data = *(stack->top);
+    
+    stack->top--;
+    stack->size--;
+    
+    printf("Popped: %c\n", data);
+    return data;
 }
 
 
