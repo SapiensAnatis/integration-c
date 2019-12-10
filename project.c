dd0b56ca960e57e97ffbba6d13fe176d0ca69ac3
Jay <jay0@me.com>
2019-12-10 14:30:07 +0000

    Implement menu function

--- project.c
+++ project.c
@@ -3,10 +3,30 @@
 #include <math.h>
 
 /*
- * Function menu()
- * -------------------
+ * --------------------------
+ * Function: menu()
+ * --------------------------
+ * Description: Displays a list of choices to the user
+ * Returns: Integer representing choice selected. Guaranteed to be either 1 or 2.
  */
 
+int menu() {
+    printf("Please select from the following options:\n\
+    \t1. Compute trapezium rule estimate\n\
+    \t2. Exit\n");
+
+    char input;
+    input = getchar();
+    input -= 48; // 48 is 0 in ASCII; by subtracting this offset, input is an integer corresponding to the chosen option's number
+    if (input >= 1 && input <= 2) { // valid range of choices
+        return input;
+    } else {
+        printf("You have selected an invalid option. Please try again.");
+        menu();
+    }
+}
+
 int main() {
-    printf("Hello world");
+    int choice;
+    choice = menu();
 }
